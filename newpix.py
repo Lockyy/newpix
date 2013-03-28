93e3910433e0097be547c868e693d128e1d19b98
Daniel Lockhart <twolocky@gmail.com>
2013-03-28 18:44:36 +0000

    Resource use decreased, accuracy increased.
    Ensures that checking loop is entered exactly on a minute change.
    Checking for whether we're at an update minute only happens once a
    minute. Once we're at an update minute we just sleep for 'interval'
    minutes between each ping.

--- newpix.py
+++ newpix.py
@@ -6,10 +6,24 @@ pygame.mixer.init()
 
 # Times to play sound at.
 updateTimes = [2,32]
+interval = 30
+
+# Get current minute
+lastMinute = datetime.datetime.now().minute
+
+# Loop till the minute advances.
+while datetime.datetime.now().minute == lastMinute:
+	pass
 
 while True:
+	# Get the datetime
 	d = datetime.datetime.now()
+	# When we hit an update time start the sound loop.
 	if d.minute in updateTimes:
-		pygame.mixer.Sound('sound.wav').play()
-	# Only check once per minute. Also ensures that sound only places once a minute.
+		# The sound loop plays the sound, then sleeps.
+		# This causes the sound to play every 'interval' minutes
+		while True:
+			pygame.mixer.Sound('sound.wav').play()
+			time.sleep(interval * 60)
+	# Only check once a minute for whether we're at an update time.
 	time.sleep(60)
