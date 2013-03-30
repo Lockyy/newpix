95d86e02642368f4302d074afc0805da9abb343c
Daniel Lockhart <twolocky@gmail.com>
2013-03-30 02:06:50 +0000

    Potential crash when url redirect being worked on fixed. Made program not save duplicate images, means I can leave running if time starts to loop so I can get a full set safely.

--- checker.py
+++ checker.py
@@ -41,9 +41,15 @@ class checker(object):
 				time.sleep(self.MINUTE)
 
 	def getRedirectURL(self, url):
-		# Get the image and store it's hash in oldHash.
-		request = urllib2.Request(url)
-		result = urllib2.urlopen(request)
+		urlObtained = False
+		while urlObtained == False:
+			try:
+				# Get the image and store it's hash in oldHash.
+				request = urllib2.Request(url)
+				result = urllib2.urlopen(request)
+				urlObtained = True
+			except:
+				pass
 		return result.geturl()
 
 	def getDateTimeString(self):
@@ -65,12 +71,25 @@ class checker(object):
 	def newImageFound(self, url, sleep = True):
 		datetime = self.getDateTimeString()
 		urlHash = url.rsplit('/', 1)[1]
-		filepath = "images/" + datetime + " " + urlHash
-		print "New image found at " + datetime
-		print "URL hash of image is " + urlHash
-		print "Image saved at " + filepath
-		pygame.mixer.Sound(self.SOUNDPATH).play()
-		image = urllib.URLopener()
-		image.retrieve(url, filepath)
-		if sleep == True:
-			time.sleep(self.NEWPIX * self.MINUTE)
+
+		imageExists = False
+
+		for image in os.listdir("images"):
+			if image[17:] == urlHash:
+				imageExists = True
+
+		if not imageExists:
+			filepath = "images/" + datetime + " " + urlHash
+			print "[" + datetime + "]"
+			print "New image found"
+			print urlHash
+			print "Image saved at " + filepath + "\n"
+			pygame.mixer.Sound(self.SOUNDPATH).play()
+			image = urllib.URLopener()
+			image.retrieve(url, filepath)
+			if sleep == True:
+				time.sleep(self.NEWPIX * self.MINUTE)
+		else:
+			print "[" + datetime + "]"
+			print "Duplicate image found."
+			print urlHash + "\n"
